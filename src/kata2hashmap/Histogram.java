2d0a6c0dc52d13dc422eb455c5ce04fafc833c2a
usuario <usuario@10.22.143.59>
2016-09-30 10:38:10 +0100

    version1

---
+++ src/kata2hashmap/Histogram.java
@@ -0,0 +1,30 @@
+package kata2hashmap;
+
+import java.util.HashMap;
+public class Histogram {
+    
+    private final int[] vector;
+
+    public Histogram(int[] vector) {
+        this.vector = vector;
+    }
+
+    public int[] getVector() {
+        return vector;
+    }
+    
+    public HashMap<Integer,Integer> getHistogram(){
+        HashMap<Integer,Integer> histo= new HashMap<>();
+        
+        for (int key : vector) {
+            if (!histo.containsKey(key)){
+                histo.put(key, 0);
+                histo.put(key, histo.get(key)+1);
+            }
+        }
+    
+    return histo;
+    }
+ 
+    
+}
